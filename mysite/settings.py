b1674b40700d6f97f87ea7403a9ab7831c6448dd
azuma <azuma20011224@gmail.com>
2021-11-25 15:48:24 +0900

    sharing post via email

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'awsloba12@gmail.com'
+EMAIL_HOST_PASSWORD = 'azuma1224'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
