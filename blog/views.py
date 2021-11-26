a461033b1711cbf4c2b8155804d411f0c2e944d4
azuma <azuma20011224@gmail.com>
2021-11-26 11:41:27 +0900

    retrieving posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.db import connection
+from django.forms.utils import flatatt
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
-from django.utils.text import slugify
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -65,11 +66,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # list of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html',
                     {'post': post,
                     'comments': comments,
                     'new_comment': new_comment,
-                    'comment_form': comment_form})
+                    'comment_form': comment_form,
+                    'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
