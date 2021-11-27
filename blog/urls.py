f274abdc986da13f1da05982ef847cb697da02cd
azuma <azuma20011224@gmail.com>
2021-11-28 03:00:59 +0900

    Adding full-text search to blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from re import search
 from django.urls import path
 from . import views
 from .feeds import LatestPostsFeed
@@ -15,4 +16,5 @@ urlpatterns = [
     views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from django.core.paginator import Paginator, EmptyPage,\
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+
 
 from taggit.models import Tag
 
@@ -13,7 +15,7 @@ from taggit.models import Tag
 from .models import Post
 from .forms import EmailPostForm
 from .models import Comment
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 
 
 def post_list(request, tag_slug=None):
@@ -98,6 +100,22 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request, 'blog/post/search.html',
+    {'form': form,
+    'query': query,
+    'results': results})
 
 class PostListView(ListView):
     queryset = Post.published.all()
